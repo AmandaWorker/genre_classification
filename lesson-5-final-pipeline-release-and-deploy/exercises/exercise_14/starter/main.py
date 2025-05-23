a4e98d6aa8f05f294f27e21e72f06caf7bd52cb2
Amanda Worker <amandaworker@Amandas-MacBook-Pro.local>
2025-05-23 15:20:08 +0100

    Fixed some errors withcheck_data

--- lesson-5-final-pipeline-release-and-deploy/exercises/exercise_14/starter/main.py
+++ lesson-5-final-pipeline-release-and-deploy/exercises/exercise_14/starter/main.py
@@ -20,7 +20,9 @@ def go(config: DictConfig):
         # This was passed on the command line as a comma-separated list of steps
         steps_to_execute = config["main"]["execute_steps"].split(",")
     else:
-        assert isinstance(config["main"]["execute_steps"], list)
+        # AW edit next two lines
+        from collections.abc import Sequence
+        assert isinstance(config["main"]["execute_steps"], Sequence)
         steps_to_execute = config["main"]["execute_steps"]
 
     # Download step
